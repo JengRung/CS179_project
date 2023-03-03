175e0aced49ec38ef497c12af765c9fca95d107c
Justin <jengrung2255@gmail.com>
2023-03-03 02:13:48 -0800

    Add "select file", "username login"

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
-from PySide2.QtWidgets import QApplication, QGridLayout, QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QWidget, QStackedWidget
+from PySide2.QtWidgets import QApplication, QGridLayout, QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QWidget, QStackedWidget, QLineEdit
 from PySide2.QtCore import Qt, QTimer
+import os
+from tkinter import filedialog as fd
+
+FILE_NAME = ""
+USERNAME = ""
 
 class MainPage(QWidget):
     def __init__(self, canvas, parent=None):
@@ -18,10 +23,22 @@ class MainPage(QWidget):
         layout.addWidget(label)
         
         # Add a button to start the game
-        button = QPushButton('Start')
-        button.setMaximumSize(150, 50)
-        button.clicked.connect(self.start_app)
-        layout.addWidget(button)
+        start_button = QPushButton('Start')
+        start_button.setMaximumSize(150, 50)
+        start_button.clicked.connect(self.start_app)
+        layout.addWidget(start_button)
+
+        # Add a button to select file
+        file_button = QPushButton('Select File')
+        file_button.setMaximumSize(150, 50)
+        file_button.clicked.connect(self.select_file)
+        layout.addWidget(file_button)
+        
+        # Add a button to login
+        login_button = QPushButton('Login')
+        login_button.setMaximumSize(150, 50)
+        login_button.clicked.connect(self.login)
+        layout.addWidget(login_button)
 
     def start_app(self):
         # Create the block grid and add it to the stacked widget
@@ -31,6 +48,14 @@ class MainPage(QWidget):
 
         # Switch to the game widget
         self.canvas.setCurrentWidget(grid)
+
+    def select_file(self):
+        global FILE_NAME
+        FILE_NAME = fd.askopenfilename()
+        print(FILE_NAME)
+        
+    def login(self):
+        self.canvas.setCurrentIndex(1) 
 
 class BlockGrid(QWidget):
     def __init__(self, rows, cols, block_size, parent=None):
@@ -126,6 +151,33 @@ class BlockGrid(QWidget):
                 col = label.property('col')
                 if (row + 1, 9 - col) == (self._track_block_x, self._track_block_y):
                     label.setStyleSheet('border: 1px solid black; background-color: red;')
+
+class LoginPage(QWidget):
+    def __init__(self, canvas, parent=None):
+        super().__init__(parent)
+        
+        self.canvas = canvas
+        
+        layout = QVBoxLayout()
+        
+        # Create a QLineEdit widget to hold the text
+        text_box = QLineEdit()
+        
+        # Create a button to confirm login
+        confirm_login = QPushButton('Log In')
+        confirm_login.setMaximumSize(150, 50)
+        confirm_login.clicked.connect(lambda: self.login(text_box.text()))
+        
+        # Add widgets to the layout
+        layout.addWidget(text_box)
+        layout.addWidget(confirm_login)
+        self.setLayout(layout)
+        
+    def login(self, username):
+        if username != '':
+            print(username)
+            USERNAME = username
+            self.canvas.setCurrentIndex(0)
                     
 class Canvas(QWidget):
     def __init__(self, parent=None):
@@ -137,6 +189,10 @@ class Canvas(QWidget):
         # Create the first page widget and add it to the stacked widget
         main_page = MainPage(self.stacked_widget)
         self.stacked_widget.addWidget(main_page)
+        
+        # Create the login page widget and add it to the stacked widget
+        login_page = LoginPage(self.stacked_widget)
+        self.stacked_widget.addWidget(login_page)
 
         # Add the stacked widget to the canvas layout
         layout = QVBoxLayout()
